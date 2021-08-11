755f198795cae5324e264135c7daece8274f015f
Jeff Hykin <jeff.hykin@gmail.com>
2021-08-11 13:09:53 -0500

    add java outline

---
+++ settings/extensions/java/#initialize.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# 
+# connect when_cleaning
+# 
+# unlink existing
+rm -f "$PROJECTR_FOLDER/settings/when_cleaning/811_java.sh" 2>/dev/null
+rm -rf "$PROJECTR_FOLDER/settings/when_cleaning/811_java.sh" 2>/dev/null
+# syslink when_cleaning
+ln -s "../extensions/java/when_cleaning.sh" "$PROJECTR_FOLDER/settings/when_cleaning/811_java.sh"
+
+
+# 
+# connect when_purging
+# 
+# unlink existing
+rm -f "$PROJECTR_FOLDER/settings/when_purging/812_purge_java.sh" 2>/dev/null
+rm -rf "$PROJECTR_FOLDER/settings/when_purging/812_purge_java.sh" 2>/dev/null
+# syslink when_purging
+ln -s "../extensions/java/when_purging.sh" "$PROJECTR_FOLDER/settings/when_purging/812_purge_java.sh"
+
+
+# 
+# connect commands
+# 
+# unlink existing
+rm -f "$PROJECTR_FOLDER/commands/tools/java" 2>/dev/null
+rm -rf "$PROJECTR_FOLDER/commands/tools/java" 2>/dev/null
+# syslink local tools
+ln -s "../../settings/extensions/java/commands" "$PROJECTR_FOLDER/commands/tools/java"
