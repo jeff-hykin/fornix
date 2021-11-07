dbb7a06fd91ee0ebc5566c42e3f46bee7f7c2f16
Jeff Hykin <jeff.hykin@gmail.com>
2021-11-06 19:12:56 -0500

    add more targets

--- settings/extensions/rust/during_start.sh
+++ settings/extensions/rust/during_start.sh
@@ -10,4 +10,13 @@ if ! rustup default 2>/dev/null 1>/dev/null
 then
     rustup install stable
     rustup default stable
+    # rustup target add wasm32-unknown-unknown    # wasm
+    # rustup target add aarch64-unknown-linux-gnu # ARM64 Linux (kernel 4.2, glibc 2.17+) 1
+    # rustup target add i686-pc-windows-gnu       # 32-bit MinGW (Windows 7+)
+    # rustup target add i686-pc-windows-msvc      # 32-bit MSVC (Windows 7+)
+    # rustup target add i686-unknown-linux-gnu    # 32-bit Linux (kernel 2.6.32+, glibc 2.11+)
+    # rustup target add x86_64-apple-darwin	      # 64-bit macOS (10.7+, Lion+)
+    # rustup target add x86_64-pc-windows-gnu     # 64-bit MinGW (Windows 7+)
+    # rustup target add x86_64-pc-windows-msvc    # 64-bit MSVC (Windows 7+)
+    # rustup target add x86_64-unknown-linux-gnu  # 64-bit Linux (kernel 2.6.32+, glibc 2.11+)
 fi
