f5e0f3f2860dcd9b4fd6d6166e9afb04df73c98f
jeff.hykin <jeff.hykin@gmail.com>
2022-02-17 09:36:34 -0600

    rust isnt broken

--- settings/extensions/nix/shell.nix
+++ settings/extensions/nix/shell.nix
@@ -142,7 +142,7 @@ let
             # Certain Rust tools won't work without this
             # This can also be fixed by using oxalica/rust-overlay and specifying the rust-src extension
             # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela. for more details.
-            RUST_SRC_PATH = "${mainPackagesIncludingRust.rust.packages.stable.rustPlatform.rustLibSrc}";
+            # RUST_SRC_PATH = "${mainPackagesIncludingRust.rust.packages.stable.rustPlatform.rustLibSrc}";
             # RUSTC_VERSION = version;
             # https://github.com/rust-lang/rust-bindgen#environment-variables
             LIBCLANG_PATH = main.packages.lib.makeLibraryPath [ main.packages.llvmPackages_latest.libclang.lib ];
