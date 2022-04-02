d0cf0c78ca8d8507e352fb060e941540ba46632f
Jeff Hykin <jeff.hykin@gmail.com>
2022-04-02 13:51:51 -0500

    add packup fallback

--- settings/during_start/092_000_install_deno_packup.sh
+++ settings/during_start/092_000_install_deno_packup.sh
@@ -1,1 +1,6 @@
-deno run -A https://deno.land/x/packup@v0.1.12/install.ts
+__temp_filepath="$HOME/.deno/bin/packup"
+if [ ! -f "$__temp_filepath" ] || [ ! -r "$__temp_filepath" ] && [ ! -x "$__temp_filepath" ]
+then
+    deno run -A https://deno.land/x/packup@v0.1.12/install.ts
+fi
+unset __temp_filepath
