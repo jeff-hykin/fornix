91148755256cfd38bd672367db6608ea82088bd6
Jeff Hykin <jeff.hykin@gmail.com>
2022-06-16 20:35:19 -0500

    -

--- main/backend/main.rs
+++ main/backend/main.rs
@@ -11,46 +11,45 @@ fn main() {
         .expect("error while running tauri application");
 }
 
+// // use deno_core::op;
+// // use deno_core::Extension;
+// // use deno_core::JsRuntime;
+// // use deno_core::RuntimeOptions;
 
-use deno_core::op;
-use deno_core::Extension;
-use deno_core::JsRuntime;
-use deno_core::RuntimeOptions;
+// // // This is a hack to make the `#[op]` macro work with
+// // // deno_core examples.
+// // // You can remove this:
+// // use deno_core::*;
 
-// This is a hack to make the `#[op]` macro work with
-// deno_core examples.
-// You can remove this:
-use deno_core::*;
+// // #[op]
+// // fn op_sum(nums: Vec<f64>) -> Result<f64, deno_core::error::AnyError> {
+// //     // Sum inputs
+// //     let sum = nums.iter().fold(0.0, |a, v| a + v);
+// //     // return as a Result<f64, AnyError>
+// //     Ok(sum)
+// // }
 
-#[op]
-fn op_sum(nums: Vec<f64>) -> Result<f64, deno_core::error::AnyError> {
-    // Sum inputs
-    let sum = nums.iter().fold(0.0, |a, v| a + v);
-    // return as a Result<f64, AnyError>
-    Ok(sum)
-}
+// // fn eval(
+// //     context: &mut JsRuntime,
+// //     code: &str,
+// // ) -> Result<serde_json::Value, String> {
+// //     let res = context.execute_script("<anon>", code);
+// //     match res {
+// //         Ok(global) => {
+// //             let scope = &mut context.handle_scope();
+// //             let local = v8::Local::new(scope, global);
+// //             // Deserialize a `v8` object into a Rust type using `serde_v8`,
+// //             // in this case deserialize to a JSON `Value`.
+// //             let deserialized_value = serde_v8::from_v8::<serde_json::Value>(scope, local);
 
-fn eval(
-    context: &mut JsRuntime,
-    code: &str,
-) -> Result<serde_json::Value, String> {
-    let res = context.execute_script("<anon>", code);
-    match res {
-        Ok(global) => {
-            let scope = &mut context.handle_scope();
-            let local = v8::Local::new(scope, global);
-            // Deserialize a `v8` object into a Rust type using `serde_v8`,
-            // in this case deserialize to a JSON `Value`.
-            let deserialized_value = serde_v8::from_v8::<serde_json::Value>(scope, local);
-
-            match deserialized_value {
-                Ok(value) => Ok(value),
-                Err(err) => Err(format!("Cannot deserialize value: {:?}", err)),
-            }
-        }
-        Err(err) => Err(format!("Evaling error: {:?}", err)),
-    }
-}
+// //             match deserialized_value {
+// //                 Ok(value) => Ok(value),
+// //                 Err(err) => Err(format!("Cannot deserialize value: {:?}", err)),
+// //             }
+// //         }
+// //         Err(err) => Err(format!("Evaling error: {:?}", err)),
+// //     }
+// // }
 
 // #[tauri::command]
 // fn run_deno(invoke_message: String) {
--- main/frontend/main.js
+++ main/frontend/main.js
@@ -7,4 +7,10 @@ for (let each of ["name_one", "Name Two", "Example-Three", "name4"]) {
 
 document.body.innerHTML += `<br>${Object.keys(globalThis.tauriApi)}`
 
+const { invoke } = globalThis.tauriApi
+invoke('deno_run', { invokeMessage: 'console.log("howdy from deno!")' }).then(thing=>{
+    document.body.innerHTML += `thing: ${thing}`
+})
+
+
 console.log("Howdy!")
