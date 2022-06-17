00a0c8216f13fcf3b2a2504c4e196905771138d6
Jeff Hykin <jeff.hykin@gmail.com>
2022-06-17 10:00:37 -0500

    consolidate run_deno

--- main/backend/main.rs
+++ main/backend/main.rs
@@ -24,7 +24,6 @@ fn main() {
 use std::env;
 use std::ptr;
 
-use deno_core::op;
 use deno_core::v8;
 use deno_core::Extension;
 use deno_core::JsRuntime;
@@ -37,54 +36,42 @@ use std::{thread, time};
 static mut RUNTIME_PTR: *mut JsRuntime = ptr::null::<JsRuntime>() as *mut JsRuntime;
 static mut RUNTIME_IN_USE: bool = false;
 
-// // This is a hack to make the `#[op]` macro work with
-// // deno_core examples.
-// // You can remove this:
-// use deno_core::*;
+use deno_core::op;
+use deno_core::*;
 
-// #[op]
-// fn op_sum(nums: Vec<f64>) -> Result<f64, deno_core::error::AnyError> {
-//     // Sum inputs
-//     let sum = nums.iter().fold(0.0, |a, v| a + v);
-//     // return as a Result<f64, AnyError>
-//     Ok(sum)
-// }
-
-fn eval(
-    context: &mut JsRuntime,
-    code: &String,
-) -> String {
-    let res = context.execute_script("<anon>", code);
-    match res {
-        Ok(global) => {
-            let scope = &mut context.handle_scope();
-            let local = v8::Local::new(scope, global);
-            // Deserialize a `v8` object into a Rust type using `serde_v8`,
-            // in this case deserialize to a JSON `Value`.
-            let deserialized_value = serde_v8::from_v8::<serde_json::Value>(scope, local);
-
-            match deserialized_value {
-                Ok(value) => json!({ "v": value}).to_string(),
-                Err(err) => json!({ "e": format!("{:?}", err)}).to_string(),
-            }
-        }
-        Err(err) => json!({ "e": format!("{:?}", err)}).to_string(),
-    }
+#[op]
+fn op_sum(nums: Vec<f64>) -> Result<f64, deno_core::error::AnyError> {
+    // Sum inputs
+    let sum = nums.iter().fold(0.0, |a, v| a + v);
+    // return as a Result<f64, AnyError>
+    Ok(sum)
 }
 
 #[tauri::command]
-fn run_deno(invoke_message: String) -> String {
-    println!("I was invoked from JS, with this message: {}", invoke_message);
+fn run_deno(code: String) -> String {
     unsafe {
         // some primitive thread blocking (bools are threadsafe right?)
         while RUNTIME_IN_USE {
             thread::sleep(time::Duration::from_millis(50));
         }
         RUNTIME_IN_USE = true;
-        let output = eval(
-            &mut (*RUNTIME_PTR),
-            &invoke_message
-        );
+        let mut context = &mut (*RUNTIME_PTR);
+        let result = context.execute_script("<anon>", &code);
+        let output = match result {
+            Ok(global) => {
+                let scope = &mut context.handle_scope();
+                let local = v8::Local::new(scope, global);
+                // Deserialize a `v8` object into a Rust type using `serde_v8`,
+                // in this case deserialize to a JSON `Value`.
+                let deserialized_value = serde_v8::from_v8::<serde_json::Value>(scope, local);
+
+                match deserialized_value {
+                    Ok(value) => json!({ "v": value}).to_string(),
+                    Err(err) => json!({ "e": format!("{:?}", err)}).to_string(),
+                }
+            }
+            Err(err) => json!({ "e": format!("{:?}", err)}).to_string(),
+        };
         RUNTIME_IN_USE = false;
         output
     }
